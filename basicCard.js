fec5e5e074288e27b77f49218a8452b354d45991
Kevin <ktorres93@knights.ucf.edu>
2017-06-20 17:44:32 -0400

    initialized npm/ inquierer installed/ questions set up

---
+++ basicCard.js
@@ -0,0 +1,9 @@
+var inquirer = require('inquirer');
+var questions =
+        { front:"The two kinds of monosaccharides are ...",
+        back:"aldoses, ketoses"
+        },
+        {
+        front: "Starches are polymers made exclusively from ...",
+        back:"glucose"
+        };
